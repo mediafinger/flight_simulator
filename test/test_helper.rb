e4e0ad86848bc29ad6458f716de09098e805b00b
Andreas Finger <andy@mediafinger.com>
2015-07-15 00:25:12 +0200

    Turn on colorful test output

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,8 @@ require "minitest/autorun"
 
 # to have colorful output, first run: gem install minitest-reporters
 # then uncomment the following 2 lines:
-# require "minitest/reporters"
-# Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true)]
+require "minitest/reporters"
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true)]
 
 require_relative "../airport"
 require_relative "../map"
